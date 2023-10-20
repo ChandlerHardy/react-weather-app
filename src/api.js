2c612ff9a8fbaa13516b05da7d707edadf48b03f
chandler-0411 <hardych04@gmail.com>
2023-10-20 00:06:24 -0500

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "82b9d6b007msha3c78d01a3fe437p1ad9d8jsn12a51f34dbd7",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "41f72862d7de0d785e88d885c01d0f4a";
